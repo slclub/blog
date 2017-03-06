4ec4b106530971643101a88d6f84f97e08a09785
slclub@163.com <root@gr9wtmtu4d.(none)>
2017-03-07 00:14:00 +0800

    fix

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -2914,7 +2914,7 @@ function wp_trim_excerpt( $text = '' ) {
 		 *
 		 * @param int $number The number of words. Default 55.
 		 */
-		$excerpt_length = apply_filters( 'excerpt_length', 55 );
+		$excerpt_length = apply_filters( 'excerpt_length', 200 );
 		/**
 		 * Filter the string in the "more" link displayed after a trimmed excerpt.
 		 *
